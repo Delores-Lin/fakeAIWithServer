faa8e7279f2f681a6802cec869fb4d7d538b4827
Delores-Lin <linzhibo6@gmail.com>
2025-04-29 00:00:12 +0800

    initdeepseek.js'

---
+++ services/deepseek.js
@@ -0,0 +1,20 @@
+import OpenAI from "openai";
+
+const DEEPSEEK_KEY = process.env.DEEPSEEK_API_KEY;
+const base_url = https://api.deepseek.com;
+
+const openai = new OpenAI({
+        baseURL: 'https://api.deepseek.com',
+        apiKey: DEEPSEEK_KEY,
+});
+
+async function sendMessageToDeepseek(history,model) {
+  const completion = await openai.chat.completions.create({
+    messages: history,
+    model: model,
+  });
+
+  return completion.choices[0].message.content;
+}
+
+module.export = {sendMessageToDeepseek };
